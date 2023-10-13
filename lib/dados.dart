15e2bcfcbb07d87860fe0bcc1d61b7d0360416b8
Gustavo <prestesgustavo999@gmail.com>
2023-10-12 23:17:54 -0400

    first commit

---
+++ lib/dados.dart
@@ -0,0 +1,112 @@
+List quiz = [
+  {
+    // 1
+    "pergunta": " Qual é o nome do protagonista da série Hora de Aventura do Cartoon Network?",
+    "respostas": [
+      "Finn",
+      "Jake",
+      "Mordecai",
+      "Rigby"
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    // 2
+    "pergunta": "Qual personagem é um cientista que é conhecido por criar várias invenções malucas em O Laboratório de Dexter?",
+    "respostas": [
+      "Dexter",
+      "Mandark",
+      "Dee Dee",
+      "Johnny Bravo"
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    // 3
+    "pergunta": "Quem é a protagonista de As Meninas Superpoderosas e é conhecida por sua cor-de-rosa e superforça?",
+    "respostas": [
+      "Lindinha",
+      "Docinho",
+      "Florzinha",
+      "Vaca e Frango"
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    // 4
+    "pergunta": "Qual é o nome do vilão que é o arqui-inimigo do Ben 10 na série homônima?",
+    "respostas": [
+      "Dr. Animo",
+      "Kevin Levin",
+      "Vilgax",
+      "Hex"
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    // 5
+    "pergunta": "Qual personagem é um garoto inteligente que tem um cachorro falante chamado Jake e vive em um mundo pós-apocalíptico em Hora de Aventura?",
+    "respostas": [
+      "Finn",
+      "Rigby",
+      "Johnny Bravo",
+      "Billy e Mandy"
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    // 6
+    "pergunta": "Quem é o protagonista de Apenas um Show e é um guaxinim azul que trabalha em um parque?",
+    "respostas": [
+      "Mordecai",
+      "Rigby",
+      "Gumball",
+      "Clarence"
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    // 7
+    "pergunta": "Qual é o nome do menino que tem uma namorada chamada Susie e gosta de fazer pegadinhas em O Incrível Mundo de Gumball?",
+    "respostas": [
+      "Darwin",
+      "Gumball",
+      "Rigby",
+      "Clarence"
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    // 8
+    "pergunta": "Quem é a personagem principal de Steven Universo e possui uma joia mágica em sua barriga?",
+    "respostas": [
+      "Pérola",
+      "Ametista",
+      "Garnet",
+      "Steven"
+    ],
+    "alternativa_correta": 3,
+  },
+  {
+    // 9
+    "pergunta": "Qual é o nome do protagonista da série A Vaca e o Frango, que é uma vaca e tem um irmão chamado Frango?",
+    "respostas": [
+      "Vaca",
+      "Frango",
+      "Johnny Bravo",
+      "Macaco Louco"
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    // 10
+    "pergunta": "Quem é o personagem principal de O Laboratório de Dexter e é um jovem cientista superdotado?",
+    "respostas": [
+      "Mandark",
+      "Dee Dee",
+      "Johnny Bravo",
+      "Dexter"
+    ],
+    "alternativa_correta": 3,
+  },
+];
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/dados.dart'; 
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentQuestionIndex = 0;
+
+  void nextQuestion() {
+    if (currentQuestionIndex < quiz.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final questao = quiz[currentQuestionIndex]["pergunta"] as String;
+    final answers = quiz[currentQuestionIndex]["respostas"] as List<String>;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 51, 255, 238),
+                  Color.fromARGB(255, 0, 255, 128),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Questão ${currentQuestionIndex + 1}',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white ,
+                      )
+                      ),
+                SizedBox(height: 40,),
+                Center(child: Text(questao, style: const TextStyle(fontSize: 20))),
+                SizedBox(height: 20),
+                Column(
+                  children: List<Widget>.generate(answers.length, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          nextQuestion();
+                        },
+                        child: Container(
+                          width: 200,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: [
+                                Colors.purple,
+                                Colors.cyan,
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(child: Text(answers[index])),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
